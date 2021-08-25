3a58ef0818737374f400770ba952984f69b87110
Guillermo Cabral <guille.cabral87@gmail.com>
2021-08-25 16:58:22 -0400

    8/25/21

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,23 @@
 //controller actions
 const User = require('../models/User');
 
+//handle errors
+const handleErrors = (err) =>{
+    console.log(err.message, err.code);
+    let errors = {email: '', password: ''};
+
+    if(err.code == 11000){
+        errors.email = 'that email is already in use!';
+        return errors;
+    }
+
+    if(err.message.includes('user validation failed')){
+        Object.values(err.errors).forEach(({properties}) =>{
+            errors[properties.path] = properties.message
+        })
+    }
+}
+
 module.exports.signup_get = (req, res) =>{
     res.render('signup');
 }
@@ -16,8 +33,8 @@ module.exports.signup_post = async (req, res) =>{
        res.status(201).json(user);
     }
     catch(err){
-        console.log(err);
-        res.status(400).send("error, user wasn't created")
+        const errors = handleErrors(err);
+        res.status(400).json({errors})
     }
 }
 
